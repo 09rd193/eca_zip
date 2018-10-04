fa8a841f9b501edd00eef4c8d4594e807bd1fd69
sonden193 <09rd193@gmail.com>
2018-10-04 23:34:14 +0900

    half way commit

--- myzip.py
+++ myzip.py
@@ -1,7 +1,18 @@
 # coding: utf-8
 
+path = './lenna.png'
+
+def gethexdata():
+  data = ''
+  with open(path, mode='rb') as f:
+    data = f.read().hex()
+  return data
+
 def main():
+  hexdata = gethexdata()
+  intdata = int(hexdata, 16)
   print('test')
+  
 
 if __name__ == '__main__':
   main()
