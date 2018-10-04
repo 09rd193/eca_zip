694923651e6519b594b3056901d763463eba7f2e
sonden193 <09rd193@gmail.com>
2018-10-04 17:52:42 +0900

    make simplify eca(rule90)

---
+++ main.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+n = 4
+size = 2 ** n 
+
+def getbintoint(integer, size):
+  binnum = format(integer, 'b')
+  binnum = binnum.zfill(size)
+  return binnum
+
+def onestep(line):
+  newline = ''
+  for i in range(len(line)):
+    x, z = '0', '0'
+    if i == 0:
+        x, z = '0', line[i + 1]
+    elif i == len(line) - 1:
+        x, z = line[i - 1], '0'
+    else:
+        x, z = line[i - 1], line[i + 1]
+    newline += '0' if x == z else '1'
+  return newline
+
+def main():
+  binnum = getbintoint(1, size)
+  for i in range((2 ** (n + 1)) - 2):
+    print(i, int(binnum, 2))
+    binnum = onestep(binnum)
+
+if __name__ == '__main__':
+  main()
