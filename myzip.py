e4984912206faa071277042b4c66c78cd0d7ff02
sonden193 <09rd193@gmail.com>
2018-10-06 11:15:01 +0900

    halfway commit
    sqliteの限界でSQL句やその返り値に文字数制限があるため数万文字のやりとりは難しいため，このアプローチは断念する

--- myzip.py
+++ myzip.py
@@ -1,17 +1,51 @@
 # coding: utf-8
+import sqlite3
 
-path = './lenna.png'
+datapath = './lenna.png'
+dbpath = 'eca.sqlite'
 
-def gethexdata():
+def selectx10(intdata):
+  connection = sqlite3.connect(dbpath)
+  cursor = connection.cursor()
+  query = '''
+    select id, x10
+    from eca_n16
+    where cast(x10 as singed) <= %s
+    order by cast(x10 as signed) desc
+    limit 1;
+  ''' % intdata
+  cursor.execute(query)
+
+  id, x10 = 0, 0
+  for i, x in cursor:
+    id, x10 = i, x
+
+  connection.close()
+  return id, int(x10)
+
+def gethexdata(datapath):
   data = ''
-  with open(path, mode='rb') as f:
+  with open(datapath, mode='rb') as f:
     data = f.read().hex()
   return data
 
 def main():
-  hexdata = gethexdata()
+  hexdata = gethexdata(datapath)
   intdata = int(hexdata, 16)
-  print('test')
+  _id, _x10 = -1, -1
+  count = 0
+  while 0 < intdata:
+    print(count)
+    count = count + 1
+    print(len(str(intdata)))
+    if _x10 < 0:
+      _id, _x10 = selectx10(intdata)
+      intdata = intdata - _x10
+    elif _x10 <= intdata:
+      intdata = intdata - _x10
+    else:
+      _id, _x10 = selectx10(intdata)
+      intdata = intdata - _x10
   
 
 if __name__ == '__main__':
